c4393b78fcff5c043d78fdf8f951c33f87430128
Blucas0707 <lucas.yuchun.lin@gmail.com>
2021-09-27 17:33:05 +0800

    fix: update dockerfile and docker for sql

--- Dockerfile.sql
+++ Dockerfile.sql
@@ -1,13 +1,10 @@
 # syntax=docker/dockerfile:1
-
 FROM mysql
 ENV MYSQL_ROOT_PASSWORD=123456 \
     MYSQL_DATABASE=chatroom
 
-
+# docker-entrypoint-initdb.d will execute .sql files
 ADD chatroom_chatroom_list.sql /docker-entrypoint-initdb.d 
 ADD chatroom_userinfo.sql /docker-entrypoint-initdb.d
 
 EXPOSE 3306
-# COPY chatroom_chatroom_list.sql /
-# COPY chatroom_userinfo.sql /
